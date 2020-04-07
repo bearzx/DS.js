6708ef59a9c7e1068046cfd0fe66eabdcc36293f
bearzx <bearzx@gmail.com>
2020-04-07 17:15:34 -0400

    tweak gif freezing

--- docs/assets/js/gif.js
+++ docs/assets/js/gif.js
@@ -1,7 +1,29 @@
-window.onload = function() {
-    let gifs = document.querySelectorAll('.gif');
-    giframes = [];
-    for (let gif of gifs) {
-        giframes.push(new Freezeframe(gif, { trigger: 'click', overlay: true }));
+function freezeGifs() {
+    /*
+      The short pause allows any required callback functions
+      to execute before actually highlighting, and allows
+      the JQuery $(document).ready wrapper to finish.
+     */
+    setTimeout(function() {
+        let gifs = document.querySelectorAll('.gif');
+        giframes = [];
+        for (let gif of gifs) {
+            giframes.push(new Freezeframe(gif, { trigger: 'click', overlay: true }));
+        }
+    }, 200);
+}
+
+/*
+  Only trigger the highlighter after document fully loaded.  This is
+  necessary for cases where page load takes a significant length
+  of time to fully load.
+*/
+if (document.readyState == 'complete') {
+    freezeGifs();
+} else {
+    document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+            freezeGifs();
+        }
     }
-};
+}
